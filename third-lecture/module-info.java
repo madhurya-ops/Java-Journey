c1ccca52c33fdc35c549652b00cf61957f09d6d8
Madhurya Mishra <65159266+madhurya-ops@users.noreply.github.com>
2024-06-12 19:50:40 +0530

    Delete third-lecture/module-info.java

--- third-lecture/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.maddy.third_tutorial {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.maddy.third_tutorial to javafx.fxml;
-    exports com.maddy.third_tutorial;
-}
