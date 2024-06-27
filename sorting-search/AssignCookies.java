6accd51331d1a75dda6267a4a442fd05db88fcc3
Madhurya Mishra <65159266+madhurya-ops@users.noreply.github.com>
2024-06-27 20:23:29 +0530

    Add files via upload

---
+++ sorting-search/AssignCookies.java
@@ -0,0 +1,14 @@
+package Assignments;
+
+import java.util.Arrays;
+
+public class AssignCookies {
+    public static void main(String[] args) {
+        int[] children = {3, 2, 1};
+        int[] cookies = {1, 1};
+
+        System.out.println(satisfyNeeds(children, cookies));
+        
+    }
+    
+  
