946792f4b00c71893ee49b2106c1a9214810e604
Madhurya Mishra <65159266+madhurya-ops@users.noreply.github.com>
2024-07-17 22:43:07 +0530

    Create Subsets.java

---
+++ Maths/Bitwise Operators/Subsets.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/subsets/submissions/1324348641/
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < (1 << n); i++) {
+            List<Integer> subset = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) > 0) {
+                    subset.add(nums[j]);
+                }
+            }
+            result.add(subset);
+        }
+        return result;
+    }
+}
