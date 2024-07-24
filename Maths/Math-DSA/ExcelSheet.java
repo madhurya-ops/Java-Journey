641649ff91a6c5cdd23f636a0e3b54494e608a0b
Madhurya Mishra <65159266+madhurya-ops@users.noreply.github.com>
2024-07-24 22:11:27 +0530

    Create ExcelSheet.java

---
+++ Maths/Math-DSA/ExcelSheet.java
@@ -0,0 +1,6 @@
+//https://leetcode.com/problems/excel-sheet-column-title/description/
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        return columnNumber == 0 ? "" : convertToTitle(--columnNumber / 26) + (char)('A' + (columnNumber % 26));
+    }
+}
