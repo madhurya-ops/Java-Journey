c1167ff258d9d3b85440d954d2a1e069ead35adb
Madhurya Mishra <65159266+madhurya-ops@users.noreply.github.com>
2024-07-27 22:21:36 +0530

    Create ConsistentStrings.java

---
+++ Maths/Bitwise Operators/ConsistentStrings.java
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/count-the-number-of-consistent-strings/
+class Solution {
+    public int countConsistentStrings(String allowed, String[] words) {
+        int count = words.length;
+	int[] store = new int[26];
+
+	for (char c : allowed.toCharArray()){
+		store[c-'a']++;
+	}
+
+	for (String word : words) {
+		
+		for (char c : word.toCharArray()) {
+			if (store[c-'a'] <= 0){
+				count--;
+				break;
+			}
+		}
+	}
+	return count;
+    }
+}
