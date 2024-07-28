abfb368f4d2c8d9214e26e34479952bffe6e11d0
Madhurya Mishra <65159266+madhurya-ops@users.noreply.github.com>
2024-07-28 22:39:55 +0530

    Add files via upload

---
+++ recursion/Ascii.java
@@ -0,0 +1,8 @@
+package com.maddy.patterns.strings;
+//to calc the ascii value of the characters
+public class Ascii {
+    public static void main(String[] args) {
+        char ch = 'a';
+        System.out.println(ch + 0);
+    }
+}
